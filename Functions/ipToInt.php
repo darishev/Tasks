260ffa4d485c09c9ab4199d632de3e302998bfec
darishev <97835745+darishev@users.noreply.github.com>
2022-03-22 00:04:35 +0300

    Update ipToInt.php

--- Functions/ipToInt.php
+++ Functions/ipToInt.php
@@ -1,4 +1,14 @@
 <?php
-
-function ipToInt(string $ip):string
-
+function ipToInt( $ip)
+{
+    $result=[];
+    $ipArray = explode('.',$ip);
+   //var_dump($ipArray);
+    $summArray = [];
+ //$resul = (128*256**3)+(32*256**2) + (10*256**1) +(1*256**0);
+    $summArray = $ipArray[0]*256**3;
+     var_dump($summArray);
+    echo (128*256**3);
+}
+ipToInt('128.32.10.1');
+//128.32.10.1
