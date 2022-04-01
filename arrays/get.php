6a2fe3eff930fc3c96a80d866ffadd71ac45ceda
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 15:21:23 +0300

    Update get.php

--- arrays/get.php
+++ arrays/get.php
@@ -1,19 +1,11 @@
 <?php
-    $cities = ['moscow', 'london', 'berlin', 'porto', null];
     function get(array $cities,int $index,$default = null):string
     {
         if($index < count($cities)){
-            echo $cities[$index] .'<br>';
+            return $cities[$index];
         }
-        else
-        {
+        else{
             $cities[$index] = $default;
             return $cities[$index];
         }
     };
-
-
-    get($cities, 1); // london
-    get($cities, 10, 'paris'); // paris
-    get($cities, 4); // null
-    get($cities, 4, 'default'); // null
