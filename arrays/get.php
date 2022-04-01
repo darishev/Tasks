2c89a3d9c66ebe5d9952a9f8a207ca0ad7ddb0c9
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 15:32:04 +0300

    Update get.php

--- arrays/get.php
+++ arrays/get.php
@@ -4,7 +4,6 @@ function get(array $cities, int $index, $default = null): string
     if ($index < count($cities)) {
         return $cities[$index];
     } else {
-        $cities[$index] = $default;
-        return $cities[$index];
+        return $default;
     }
 }
