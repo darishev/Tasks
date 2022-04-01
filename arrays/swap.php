dc31cd0daac432ea8d763776eab55c6aad1f9a2f
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 15:30:53 +0300

    Update swap.php

--- arrays/swap.php
+++ arrays/swap.php
@@ -1,27 +1,13 @@
 <?php
-$names = ['john', 'smith', 'karl'];
+function swap(array $names, int $index): array
+{
+    if (isset($names[$index - 1]) and isset($names[$index + 1])) {
+        $tempData = $names;
+        $names[$index - 1] = $tempData[$index + 1];
+        $names[$index + 1] = $tempData[$index - 1];
+        return $names;
+    } else {
+        return $names;
+    }
+}
 
- function swap (array $names,int $index):array
-    {
-        if (isset($names[$index-1]) and isset($names[$index+1]) )
-        {
-            $tempData = $names;
-            $names[$index-1] = $tempData[$index+1];
-            $names[$index+1] = $tempData[$index-1];
-            return $names;
-        }
-        else
-        {
-            return $names;
-         }
-    };
-
-
-$result = swap($names, 1);
-print_r($result); // => ['karl', 'smith', 'john']
-
-$result = swap($names, 2);
-print_r($result); // => ['john', 'smith', 'karl']
-
-$result = swap($names, 0);
-print_r($result); // => ['john', 'smith', 'karl']
