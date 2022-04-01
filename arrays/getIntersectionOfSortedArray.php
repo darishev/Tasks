27723ca7ede423bcec4bedbf16ea5dd99d1244b4
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 15:29:24 +0300

    Update getIntersectionOfSortedArray.php

--- arrays/getIntersectionOfSortedArray.php
+++ arrays/getIntersectionOfSortedArray.php
@@ -1,18 +1,15 @@
 <?php
-    function getIntersectionOfSortedArray(array $firstArray,array $secondArray):array
-    {
+function getIntersectionOfSortedArray(array $firstArray, array $secondArray): array
+{
 
-        $lenghtFirstArray = count($firstArray);
-        $lenghtSecondArray = count($secondArray);
-            //3i * 6i
-            for($i=0;$i<$lenghtFirstArray;$i++){
-                for($f=0;$f<$lenghtSecondArray;$f++){
-                    if($firstArray[$i]===$secondArray[$f])
-                    {
-                        return $firstArray[$i].'<br>';
-                    }
-                 }
+    $lenghtFirstArray = count($firstArray);
+    $lenghtSecondArray = count($secondArray);
+    //3i * 6i
+    for ($i = 0; $i < $lenghtFirstArray; $i++) {
+        for ($f = 0; $f < $lenghtSecondArray; $f++) {
+            if ($firstArray[$i] === $secondArray[$f]) {
+                return $firstArray[$i] . '<br>';
             }
+        }
     }
-
-getIntersectionOfSortedArray([10, 11, 24], [10, 13, 14, 18, 24, 30]); // [10, 24]
+}
