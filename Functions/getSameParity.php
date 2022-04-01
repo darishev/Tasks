944bc49bd25ef8fe7a46b18929940d2c5dce74e8
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 20:10:24 +0300

    Update getSameParity.php

--- Functions/getSameParity.php
+++ Functions/getSameParity.php
@@ -1,12 +1,12 @@
 <?php
-    function getSameParity(array $inputArray=[]):array
-    {
-        $evenNums = [];
-        for($i=0;$i<count($inputArray);$i++){
-            if($inputArray[$i]%2!==0){
-                array_push($evenNums, $inputArray[$i]);
-            }
+function getSameParity(array $inputArray = []): array
+{
+    $evenNums = [];
+    for ($i = 0; $i < count($inputArray); $i++) {
+        if ($inputArray[$i] % 2 !== 0) {
+            array_push($evenNums, $inputArray[$i]);
         }
-        return $evenNums;
-
     }
+    return $evenNums;
+
+}
