060d1bcd5a29228c7bd333ae53522d47421a643b
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 15:18:44 +0300

    Update findIndexOfNearest.php

--- Functions/findIndexOfNearest.php
+++ Functions/findIndexOfNearest.php
@@ -7,14 +7,11 @@
             $tempData = $inputArray[$i]-$needledNum;
             array_push($goodResults,$tempData);
             asort($goodResults);
-
         }
     } else{
-        return (null);
+        return null;
     }
 
         return key(($goodResults));
     }
 
-findIndexOfNearest([1, 10, 0, 4], 0); // 2
-findIndexOfNearest([], 2); // null
