10fabe14a3ef66bdd76c11d4308fe010098742ac
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 15:28:17 +0300

    Update addPrefix.php

--- arrays/addPrefix.php
+++ arrays/addPrefix.php
@@ -1,15 +1,13 @@
 <?php
-    $names = ['john', 'smith', 'karl'];
 
-     function addPrefix($names,$prefix = null):string
-     {
-         if(isset($names) and $prefix){
-             for ($i = 0; $i < count($names); $i++) {
-                 $newNames= "{$prefix} {$names[$i]}";
-                 return $newNames;
-             }
-         }
-         else{
-                return'Префикс отсуствует';
-         }
-     }
+function addPrefix($names, $prefix = null): string
+{
+    if (isset($names) and $prefix) {
+        for ($i = 0; $i < count($names); $i++) {
+            $newNames = "{$prefix} {$names[$i]}";
+            return $newNames;
+        }
+    } else {
+        return 'Префикс отсуствует';
+    }
+}
