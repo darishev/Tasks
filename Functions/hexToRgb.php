2c008f4bf6657da03f6a96d4e7fb4fa1fd1bd981
darishev <97835745+darishev@users.noreply.github.com>
2022-04-01 22:39:58 +0300

    Update hexToRgb.php

--- Functions/hexToRgb.php
+++ Functions/hexToRgb.php
@@ -1,12 +1,15 @@
 <?php
-function hexToRgb(int $inputString): array
+function hexToRgb(string $inputString): array
 {
+    $hexedInput = hexdec($inputString);
     $offsetStep = [16, 8, 0];
     $Keys = [];
-    for ($i = 0, $x = 8; $i < 3; $i++) {
-        $step[$i] = (($inputString >> $offsetStep[$i]) & 0xFF);
+    for ($i = 0; $i < 3; $i++) {
+        $step[$i] = (($hexedInput >> $offsetStep[$i]) & 0xFF);
         array_push($Keys, $step[$i]);
+
     }
+
     $RGB = [
         'R' => $Keys[0],
         'G' => $Keys[1],
@@ -15,7 +18,3 @@ function hexToRgb(int $inputString): array
     return $RGB;
 }
 
-
-
-//hexToRgb(0x432366); // ['r' => 36, 'g' => 171, 'b' => 0]
-//rgbToHex([36, 171, 0]); // '#24ab00'
